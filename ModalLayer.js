fd1256a567f41bafe4ea72e1ae104eb17f9befc0
manufosela <manufosela@gmail.com>
2022-08-22 16:49:44 +0200

    fix: fix position when there are scroll y

--- ModalLayer.js
+++ ModalLayer.js
@@ -67,7 +67,7 @@ export class ModalLayer {
 
   injectModalContent() {
     this.bodyBlackedout.style.height = document.documentElement.clientHeight;
-    this.modalHTML.style.top = `calc(${window.scrollY}px + 50%)`;
+    // this.modalHTML.style.top = `calc(${window.scrollY}px + 50%)`;
     this.modalHTML.shadowRoot.innerHTML = `
       <style>
         ${this.modalStyleClose}
